e4479b8da979e0def0494a561a6ddebd918543d8
Aashi Singh <aashiishere.2004@gmail.com>
2026-03-20 01:56:23 +0530

    update

--- Backend/src/controllers/users.controllers.js
+++ Backend/src/controllers/users.controllers.js
@@ -2,39 +2,39 @@ import httpStatus from "http-status";
 import { User } from "../models/user.model.js";
 import bcrypt, { hash } from "bcrypt"
 
-// import crypto from "crypto"
-// import { Meeting } from "../models/meeting.model.js";
-// const login = async (req, res) => {
+import crypto from "crypto"
+import { Meeting } from "../models/meeting.model.js";
+const login = async (req, res) => {
 
-//     const { username, password } = req.body;
+    const { username, password } = req.body;
 
-//     if (!username || !password) {
-//         return res.status(400).json({ message: "Please Provide" })
-//     }
+    if (!username || !password) {
+        return res.status(400).json({ message: "Please Provide" })
+    }
 
-//     try {
-//         const user = await User.findOne({ username });
-//         if (!user) {
-//             return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" })
-//         }
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" })
+        }
 
 
-//         let isPasswordCorrect = await bcrypt.compare(password, user.password)
+        let isPasswordCorrect = await bcrypt.compare(password, user.password)
 
-//         if (isPasswordCorrect) {
-//             let token = crypto.randomBytes(20).toString("hex");
+        if (isPasswordCorrect) {
+            let token = crypto.randomBytes(20).toString("hex");
 
-//             user.token = token;
-//             await user.save();
-//             return res.status(httpStatus.OK).json({ token: token })
-//         } else {
-//             return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or password" })
-//         }
+            user.token = token;
+            await user.save();
+            return res.status(httpStatus.OK).json({ token: token })
+        } else {
+            return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or password" })
+        }
 
-//     } catch (e) {
-//         return res.status(500).json({ message: `Something went wrong ${e}` })
-//     }
-// }
+    } catch (e) {
+        return res.status(500).json({ message: `Something went wrong ${e}` })
+    }
+}
 
 
 const register = async (req, res) => {
